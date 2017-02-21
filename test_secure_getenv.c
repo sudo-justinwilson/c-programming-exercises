ca4c3c9197b5882bf9620300fa9031b92b7ff563
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-02-22 09:25:52 +1100

    test out some linux functions

---
+++ test_secure_getenv.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *getenv(const char *name);
+
+char *secure_getenv(const char *name);
+
+int main()
+{
+    char *env = "HOME";
+
+    char *variable;
+
+    variable = secure_getenv(env);
+
+    printf("The env is:\t %s  and it returns: \t %s \n \n", env, variable);
+
+    return 0;
+    }
+
+
